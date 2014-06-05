86b926b18ee3590dc7086f3646b004565163345f
bbklab <bbklab@126.com>
2014-06-05 15:50:42 +0800

    remove unless files for printer

--- printer/reffers/1.sh
+++
@@ -1,9 +0,0 @@
-# eyou printer  172.16.100.12
-snmpwalk -v 1 -c public 172.16.100.12  > snmp.lst
-
-#HOST-RESOURCES-MIB::hrDeviceType.1 = OID: HOST-RESOURCES-TYPES::hrDevicePrinter
-#HOST-RESOURCES-MIB::hrDeviceDescr.1 = STRING: Generic 42BW-4SeriesPCL
-#HOST-RESOURCES-MIB::hrDeviceStatus.1 = INTEGER: running(2)
-#HOST-RESOURCES-MIB::hrDeviceErrors.1 = Counter32: 0
-#HOST-RESOURCES-MIB::hrPrinterStatus.1 = INTEGER: other(1)
-#HOST-RESOURCES-MIB::hrPrinterDetectedErrorState.1 = Hex-STRING: 00
